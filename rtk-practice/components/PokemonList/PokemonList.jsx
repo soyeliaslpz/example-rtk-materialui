6e66e71b5e082209a3522ce188a68f3529e2063f
M.C <ingmaryory.a@gmail.com>
2024-01-10 18:19:32 -0400

    test

--- rtk-practice/components/PokemonList/PokemonList.jsx
+++ rtk-practice/components/PokemonList/PokemonList.jsx
@@ -7,16 +7,13 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name, url) {
+  return { name, url };
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159),
+  createData("Ice cream sandwich", 237),
 ];
 
 export default function PokemonList() {
@@ -25,11 +22,8 @@ export default function PokemonList() {
       <Table sx={{ minWidth: 650 }} aria-label='simple table'>
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align='right'>Calories</TableCell>
-            <TableCell align='right'>Fat&nbsp;(g)</TableCell>
-            <TableCell align='right'>Carbs&nbsp;(g)</TableCell>
-            <TableCell align='right'>Protein&nbsp;(g)</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell align='right'>Url</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -41,10 +35,7 @@ export default function PokemonList() {
               <TableCell component='th' scope='row'>
                 {row.name}
               </TableCell>
-              <TableCell align='right'>{row.calories}</TableCell>
-              <TableCell align='right'>{row.fat}</TableCell>
-              <TableCell align='right'>{row.carbs}</TableCell>
-              <TableCell align='right'>{row.protein}</TableCell>
+              <TableCell align='right'>{row.url}</TableCell>
             </TableRow>
           ))}
         </TableBody>
